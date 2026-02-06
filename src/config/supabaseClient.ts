175ad4f757e20ea705d70f1bfea63a3b23a20d12
aleksi loddo <143383551+Aleksi-Loddo@users.noreply.github.com>
2026-02-06 16:42:24 +0200

    commented out some code

--- src/config/supabaseClient.ts
+++ src/config/supabaseClient.ts
@@ -1,6 +1,5 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
-import * as dotenv from 'dotenv'; 
-dotenv.config(); 
+
 const SUPABASE_URL = process.env.SUPABASE_URL!;
 const ANON_KEY = process.env.SUPABASE_ANON_KEY!;
 const SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_KEY!;
@@ -33,6 +32,11 @@ export function createUserClientFromAuthHeader(authHeader?: string) {
     auth: { persistSession: false },
   }) as SupabaseClient;
 }
+
+
+
+/*import * as dotenv from 'dotenv'; 
+dotenv.config(); */
 
 
 
