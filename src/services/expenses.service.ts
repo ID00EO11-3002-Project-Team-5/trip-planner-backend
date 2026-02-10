6733375b9e8ea09eb05aa5f3a12e696ee444c4d7
Achini Fernando <akurukul24@students.oamk.fi>
2026-02-10 14:34:04 +0200

    improve expense service

--- src/services/expenses.service.ts
+++ src/services/expenses.service.ts
@@ -4,21 +4,36 @@ import { CreateExpenseInput } from "../validators/expense.schema";
 export const createExpenseService = async (
   supabase: SupabaseClient,
   payload: CreateExpenseInput,
+  userId: string,
 ) => {
-  const { data, error } = await supabase
+  // create expenses
+  const {data: expense, error: expenseError } = await supabase
     .from("t_expense_expe")
     .insert({
       id_trip: payload.id_trip,
       title_expe: payload.title_expe,
       amount_expe: payload.amount_expe,
       currency_expe: payload.currency_expe,
+      created_by: userId,
     })
     .select()
     .single();
 
-  if (error) {
-    throw new Error(error.message);
+  if (expenseError) {
+    throw new Error(expenseError.message);
+  }
+  //  Create payer record (MVP: creator paid full amount)
+  const { error: payerError } = await supabase
+    .from("t_expense_payer_expa")
+    .insert({
+      id_expe: expense.id_expe,
+      id_user: userId,
+      payeramount_expa: payload.amount_expe,
+    });
+
+  if (payerError) {
+    throw new Error(payerError.message);
   }
 
-  return data;
+  return expense;
 };
