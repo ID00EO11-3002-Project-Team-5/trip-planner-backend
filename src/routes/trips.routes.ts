1273ee4733b2bb170045f49c4014c432d949bbb0
aleksi loddo <143383551+Aleksi-Loddo@users.noreply.github.com>
2026-02-11 14:42:56 +0200

    tested  and writen trips CRUD  operations, fixed  RLS policies and added  trigger for auto  adding  users to user table

--- src/routes/trips.routes.ts
+++ src/routes/trips.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { createTrip } from "../controllers/trips.controller";
+import { createTrip,   deleteTrip, getTrip, getTrips, updateTrip} from "../controllers/trips.controller";
 
 const router = Router();
 
 router.post("/", createTrip);
+router.get("/", getTrips);
+router.get("/:id", getTrip);
+router.put("/:id", updateTrip);
+router.delete("/:id", deleteTrip);
 
 export default router;
